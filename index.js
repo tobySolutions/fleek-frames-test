50f16a64ac28e9601937218b03c66571f9f08e64
tobySolutions <adedejitobiloba7@gmail.com>
2024-10-06 20:17:37 +0100

    feat: add state handling

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ export default function bootstrap(HUB_URL) {
         <meta property="fc:frame" content="vNext" />
         <meta property="of:accepts:xmtp" content="2024-02-01" />
         <meta property="fc:frame:image" content="${imageDataUrl}" />
-        <meta property="fc:frame:image:aspect_ratio" content="1:1" />
+        <meta property="fc:frame:image:aspect_ratio" content="1.91:1"/>
         <meta property="fc:frame:post_url" content="${framePostUrl}?r=3" />
         <meta property="fc:frame:input:text" content="Enter custom fruit..." />
         <meta property="fc:frame:button:1" content="Apples" data-value="apples" />
